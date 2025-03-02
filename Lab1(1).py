4bcadf4407e819e266697dfc334d78d895221ca0
DipoA-Git <alabao0808@students.bowiestate.edu>
2025-03-02 14:56:23 -0500

    Add files via upload

---
+++ Lab1(1).py
@@ -0,0 +1,6 @@
+class1=int(input('Enter score between (0-100'))
+class2=int(input('Enter score between (0-100'))
+class3=int(input('Enter score between (0-100'))
+
+average=(class1+class2+class3)/3
+print(average)
